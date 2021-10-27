4c2b300355e41cc42e0d0afeba404786ff731c19
Olesya <petrenco.olesya@gmail.com>
2021-10-27 04:46:34 +0300

    adds hw-6

--- src/telran/util/ArrayList.java
+++ src/telran/util/ArrayList.java
@@ -37,19 +37,17 @@ public class ArrayList<T> implements List<T> {
 		if (index == size) {
 			add(element);
 			res = true;
-			
-		} else if(isValidIndex(index)) {
+
+		} else if (isValidIndex(index)) {
 			res = true;
 			if (size == array.length) {
 				allocate();
 			}
-		
 			System.arraycopy(array, index, array, index + 1, size - index);
 			array[index] = element;
 			size++;
 		}
 		return res;
-
 	}
 
 	@Override
@@ -77,16 +75,16 @@ public class ArrayList<T> implements List<T> {
 			res = array[index];
 			size--;
 			System.arraycopy(array, index + 1, array, index, size - index);
-			//FIXME regarding setting null
+			// FIXME regarding setting null
 		}
-		
-		return res;
 
+		return res;
 	}
+
 	@Override
 	public boolean contains(T pattern) {
 		boolean res = false;
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (array[i].equals(pattern)) {
 				res = true;
 				break;
@@ -94,20 +92,11 @@ public class ArrayList<T> implements List<T> {
 		}
 		return res;
 	}
-	@Override
-	public int indexOf(T pattern) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int lastIndexOf(T pattern) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+
 	@Override
 	public boolean contains(Predicate<T> predicate) {
 		boolean res = false;
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			if (predicate.test(array[i])) {
 				res = true;
 				break;
@@ -115,21 +104,69 @@ public class ArrayList<T> implements List<T> {
 		}
 		return res;
 	}
+
 	@Override
-	public int indexOf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public int lastIndexOf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	@Override
-	public boolean removeIf(Predicate<T> predicate) {
-		// TODO Auto-generated method stub
-		return false;
+	public int indexOf(T pattern) {
+		int res = -1;
+		for (int i = 0; i < size; i++) {
+			if (array[i].equals(pattern)) {
+				res = i;
+				break;
+			}
+		}
+
+		return res;
 	}
 
-	
+	@Override
+	public int lastIndexOf(T pattern) {
+		int res = -1;
+		for (int i = size - 1; i >= 0; i--) {
+			if (array[i].equals(pattern)) {
+				res = i;
+				break;
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public int indexOf(Predicate<T> predicate) {
+		int res = -1;
+		for (int i = 0; i < size; i++) {
+			if (predicate.test(array[i])) {
+				res = i;
+				break;
+			}
+		}
+		return res;
+
+	}
+
+	@Override
+	public int lastIndexOf(Predicate<T> predicate) {
+		int res = -1;
+		for (int i = size - 1; i >= 0; i--) {
+			if (predicate.test(array[i])) {
+				res = i;
+				break;
+			}
+		}
+		return res;
+	}
+
+	@Override
+	public boolean removeIf(Predicate<T> predicate) {
+		boolean res = false;
+		for (int i = 0; i < size;) {
+			if (predicate.test(array[i])) {
+				remove(i);
+				res = true;
+			}else {
+				i++;	
+			}	
+		}
+		return res;
+	}
+
 }
